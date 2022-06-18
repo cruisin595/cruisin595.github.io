2673e34040ce120af74473fc959994cc752ff456
Wshape1 <Wshape1@users.noreply.github.com>
2022-06-18 21:23:39 +0800

    Add files via upload

--- js/boxchange_jquery.js
+++ js/boxchange_jquery.js
@@ -1,22 +1,27 @@
-$(document).ready(function() {
+
+
+$(document).ready(function () {
     $(".box1").css("height", parseFloat($(".box1").css("width")) * 0.5)
     $(".box2").css("height", parseFloat($(".box2").css("width")) * 0.5)
     $(".box3").css("height", parseFloat($(".box3").css("width")) * 0.5)
     $(".box4").css("height", parseFloat($(".box4").css("width")) * 0.5)
 
-    var bot = $(".box1").children().eq(0).css("bottom")
-    alert(bot)
-    // $(".box1").append("<style>.box1 section:before{top:" + bot + "}</style>")
-    // $(".box1 section").css("top", parseFloat($(".box1::before").css("bottom")))
-    // $(".box2 section::before").css("bottom", parseFloat($(".box2::before").css("bottom")))
+    var bot1 = window.getComputedStyle(document.getElementsByClassName("box1")[0], ":before").getPropertyValue("bottom");
+    // alert(bot1);
+    var bot2 = window.getComputedStyle(document.getElementsByClassName("box2")[0], ":before").getPropertyValue("bottom");
+    // alert(bot2);
+    $(".box1").append("<style>.box1 section:before{top:" + bot1 + "}</style>")
+    $(".box2").append("<style>.box2 section:before{top:" + bot2 + "}</style>")
 })
 
-$(window).resize(function() {
+$(window).resize(function () {
     $(".box1").css("height", parseFloat($(".box1").css("width")) * 0.5)
     $(".box2").css("height", parseFloat($(".box2").css("width")) * 0.5)
     $(".box3").css("height", parseFloat($(".box3").css("width")) * 0.5)
     $(".box4").css("height", parseFloat($(".box4").css("width")) * 0.5)
 
-    // $(".box1 section::before").css("bottom", parseFloat($(".box1::before").css("bottom")))
-    // $(".box2 section::before").css("bottom", parseFloat($(".box2::before").css("bottom")))
+    var bot1 = window.getComputedStyle(document.getElementsByClassName("box1")[0], ":before").getPropertyValue("bottom");
+    var bot2 = window.getComputedStyle(document.getElementsByClassName("box2")[0], ":before").getPropertyValue("bottom");
+    $(".box1").append("<style>.box1 section:before{top:" + bot1 + "}</style>")
+    $(".box2").append("<style>.box2 section:before{top:" + bot2 + "}</style>")
 })
